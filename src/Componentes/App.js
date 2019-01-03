330491659e1f3ecad9e47974e5452c9f8c903db5
DanielMontalvan <montalvandaniel2@gmail.com>
2019-01-02 22:51:11 -0600

    segundo commit

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -1,16 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import '../Css/App.css';
-import Header from '../Componentes/Header'
+import Formulario from "./Formulario";
+import Header from "./Header";
+import Error from "./Error";
+
 class App extends Component {
+  state = {
+    error: "",
+    consulta: "",
+    retultado: ""
+  };
+
+  componentDidUpdate() {
+    this.consultarApi();
+  }
+
+  componentDidMount() {
+    this.setState({ error: false });
+  }
+  datosConsulta = respuesta => {
+    if (respuesta.ciudad === "" || respuesta.pais === "") {
+      this.setState({ error: true, consulta: "" });
+    } else {
+      this.setState({ error: false, consulta: respuesta });
+    }
+  };
+  consultarApi = () => {
+    const { ciudad, pais } = this.state.consulta;
+    if (ciudad !== undefined || pais !== undefined) {
+      console.log(ciudad);
+      const appId = "0404e413d6c971d74f7a58d6f3a0b6cc";
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+      console.log(url);
+      fetch(url)
+        .then(respuesta => {
+          return respuesta.json();
+        })
+        .then(datos => {
+          this.setState({ resultado: datos });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  };
+
   render() {
+    const error = this.state.error;
+    let resultado;
+    if (error) {
+      resultado = <Error mensaje="Ambos campos son obligatorios" />;
+    }
     return (
       <div className="App">
-        <Header className="App-header"
-          titulo='Clima React'
-        />
+        <Header className="App-header" titulo="Clima React" />
+        <Formulario datosConsulta={this.datosConsulta} />
+        {resultado}
       </div>
     );
   }
 }
 export default App;
-
---
+++ src/Componentes/Formulario.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Formulario extends Component {
+  state = {};
+  ciudadRef = React.createRef();
+  paisRef = React.createRef();
+  buscarClima = e => {
+    //Leer los ref y crear el objeto
+    e.preventDefault();
+    const respuesta = {
+      pais: this.paisRef.current.value,
+      ciudad: this.ciudadRef.current.value
+    };
+
+    //enviar por this.props.
+    this.props.datosConsulta(respuesta);
+    //opcional resetear el formulario
+  };
+
+  render() {
+    return (
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <form onSubmit={this.buscarClima}>
+              <div className="input-field col s12 m8 l4 offset-m2">
+                <input id="ciudad" ref={this.ciudadRef} type="text" />
+                <label htmlFor="ciudad ">Ciudad:</label>
+              </div>
+              <div className="input-field col s12 m8 l4 offset-m2">
+                <select ref={this.paisRef}>
+                  <option value="AR">Argentina</option>
+                  <option value="CO">Colombia</option>
+                  <option value="CR">Costa Rica</option>
+                  <option value="ES">España</option>
+                  <option value="US">USA</option>
+                  <option value="MX">Mexico</option>
+                  <option value="PE">Peru</option>
+                </select>
+                <label htmlFor="pais">País:</label>
+              </div>
+              <div className="input-field col s12 m8 l4 offset-m2 buscador ">
+                <input
+                  type="submit"
+                  className="waves-effect waves-light btn-large yellow accent-4"
+                  value="Buscar..."
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Formulario;
